ba8716d0ecaef53e4791951545f53d3f06065913
gavalierm <marcel@gavalier.sk>
2022-11-15 11:59:33 +0100

    back trigger

--- app/js/app.js
+++ app/js/app.js
@@ -19,13 +19,26 @@ $(document).on("click", "._open", async function(event) {
 
 
 
+//global array for back stepping
+var global_back_steps = new Array('_panel_library');
 
 async function openPanel(target) {
     if (checkAttr(target)) {
         console.log("openPanel", "Undefined target");
         return false;
     }
-
+    //
+    if (target == "_panel_back") {
+        var _target = '_panel_library';
+        if (global_back_steps.length > 1) {
+            _target = global_back_steps[(global_back_steps.length - 2)];
+            global_back_steps.pop();
+        }
+        return openPanel(_target);
+    } else if (target == "_panel_close") {
+        return openPanel('_panel_library');
+    }
+    //
     //remove all class with _ leading
     $("body").removeClass(function(index, css) {
         var replace = target + "\\d";
@@ -35,6 +48,13 @@ async function openPanel(target) {
 
     //add target
     $("body").addClass(target);
+    if (global_back_steps.length && global_back_steps[global_back_steps.length - 1] !== target) {
+        global_back_steps.push(target);
+    }
+    if (global_back_steps.length > 3) {
+        global_back_steps.shift();
+    }
+    //console.log(global_back_steps);
 }
 
 function checkAttr(target) {
